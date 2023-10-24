864043b91f1f195e3ed69ad9181a1afb56a60a35
Valentino Jerez <119681181+ValentinoJerez@users.noreply.github.com>
2023-10-24 18:27:31 -0300

    Revert "Mas views"

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,35 @@
-import { Route, Routes } from "react-router-dom";
-
-import Home from "./views/Home";
-import Services from "./views/Services";
-import Team from "./views/Team";
-import Contact from "./views/Contact";
-import Blog from "./views/Blog";
-
-import "./App.css";
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
 function App() {
+  const [count, setCount] = useState(0)
 
   return (
-    <div className="font-orkney">
-      <Routes>
-        <Route path="/" element = {<Home/>}/>
-        <Route path="/Services" element={<Services />}/>
-        <Route path="/Team" element={<Team />}/>
-        <Route path="/Blog" element={<Blog />}/>
-        <Route path="/Contact" element={<Contact />}/>
-        <Route path="/Login" element={<Login />}/>
-        <Route path="/Register" element={<Register />}/>
-      </Routes>
-    </div>
-  );
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
 }
 
 export default App
