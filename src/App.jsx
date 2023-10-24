4b1464b1e4006399879ec0a8cb6d3fd21a7b2029
ValentinoJerez <valenjerez69@gmail.com>
2023-10-24 18:25:13 -0300

    Blog, Contact, Login

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,25 @@
 import { Route, Routes } from "react-router-dom";
-import "./App.css";
+
 import Home from "./views/Home";
+import Services from "./views/Services";
+import Team from "./views/Team";
+import Contact from "./views/Contact";
+import Blog from "./views/Blog";
+
+import "./App.css";
 
 function App() {
 
   return (
     <div className="font-orkney">
       <Routes>
-        <Route
-          path="/"
-          element = {<Home/>}
-        />
+        <Route path="/" element = {<Home/>}/>
+        <Route path="/Services" element={<Services />}/>
+        <Route path="/Team" element={<Team />}/>
+        <Route path="/Blog" element={<Blog />}/>
+        <Route path="/Contact" element={<Contact />}/>
+        <Route path="/Login" element={<Login />}/>
+        <Route path="/Register" element={<Register />}/>
       </Routes>
     </div>
   );
