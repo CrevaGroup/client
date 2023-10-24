cfadd372ebbfa5267dd3cb676ebeb06b848a7087
AxelChoque <axel_chooque@hotmail.com>
2023-10-24 15:57:45 -0300

    structured nav

---
+++ src/views/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Navbar from "../components/Navbar";
+
+const Home = () => {
+    return(
+        <div className="bg-custom-gray h-screen">
+            <Navbar/>
+        </div>
+    )
+}
+
+export default Home;
