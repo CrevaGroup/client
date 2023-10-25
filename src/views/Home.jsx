132c9086ad8f59615d401cf84df396fa99b03e8e
AxelChoque <axel_chooque@hotmail.com>
2023-10-25 10:50:06 -0300

    nav with icons

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -3,7 +3,9 @@ import Navbar from "../components/Navbar";
 
 const Home = () => {
     return(
-        <div className="bg-custom-gray h-screen">
+        <div className="bg-custom-gray h-full">
+            <p>Creva significa “Crear Valor” el que muchas veces está ahí pero no sabemos cómo demostrarlo. Potenciamos a las personas en su búsqueda de crecimiento profesional desde cualquier parte del mundo y de diversas culturas.
+</p>
         </div>
     )
 }
