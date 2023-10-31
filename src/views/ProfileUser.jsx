b223cba72cfa6322ee15ae7bfcd4a3a74afaba81
ValentinoJerez <valenjerez69@gmail.com>
2023-10-31 13:25:19 -0300

    Perfil de Usuario responsive

--- src/views/ProfileUser.jsx
+++ src/views/ProfileUser.jsx
@@ -4,23 +4,29 @@ function ProfileUser() {
   return (
     <div>
       <div className="text-center">
-        <h1 className="text-4xl">Tu Perfil</h1>
+        <h1 className="text-4xl mb-5">Tu Perfil</h1>
       </div>
-      <div className="flex items-center justify-center ml-8 mt-8">
-        <div className="text-center mx-20">
+      <div className="flex flex-col items-center justify-center mt-8 mb-8 lg:flex-row lg:items-start lg:justify-center">
+        <div className="text-center mb-6 lg:mb-0">
           <h3 className="text-2xl mb-4">Nombre, Apellido</h3>
-          <div className="w-64 h-64 mx-auto rounded" style={{backgroundColor:`#C2A3D1`}}>
-            <div className="rounded-full overflow-hidden w-full h-full">
-              <img src="" alt="" className="w-full h-full object-cover" />
+          <div className="w-80 h-80 mx-auto rounded" style={{ backgroundColor: `#C2A3D1` }}>
+            <div className="flex items-center justify-center w-full h-full">
+              <div className="rounded-full overflow-hidden w-3/4 h-3/4">
+                <img
+                  src=""
+                  alt="Imagen de Usuario"
+                  className="w-full h-full object-cover"
+                />
+              </div>
             </div>
           </div>
         </div>
-        <div className="text-xl ml-8 mt-12">
+        <div className="text-xl mt-20 lg:ml-8">
           <h3>Edad:</h3>
           <h3>Ciudad:</h3>
           <h3>Plan:</h3>
           <h3>Email:</h3>
-          <div className="mt-8">
+          <div className="mt-12 ">
             <Button className="bg-dark-violet mr-4">Eliminar Perfil</Button>
             <Button className="bg-dark-violet">Editar</Button>
           </div>
