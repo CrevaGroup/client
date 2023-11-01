b9edf52d6c584d921a206d60f0be4c9b244c6ee1
AxelChoque <axel_chooque@hotmail.com>
2023-11-01 07:14:44 -0300

    blogsmenu

---
+++ src/components/BlogsMenu.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const BlogsMenu = () => {
+    return(
+        <div className="flex items-center justify-center w-full">
+            <div
+                className="flex flex-col bg-red-500 w-3/4 h-full items-center justify-center"
+            >
+                <p>Actualizar post</p>
+
+                <div>
+                    
+                </div>
+
+            </div>
+        </div>
+    )
+}
+
+export default BlogsMenu;
