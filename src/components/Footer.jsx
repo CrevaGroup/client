52e082ece43adde13f81465a094b8ce4c353b538
ValentinoJerez <valenjerez69@gmail.com>
2023-11-03 19:53:36 -0300

    Nuevo Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,52 +1,62 @@
 import { NavLink } from "react-router-dom";
 
-import LogoBlanco from "../assets/Blanco-transparente-Logo.png"
+import Logo from "../assets/logo.png"
+import Linkedin from "../assets/MdiLinkedin.svg"
+import Instagram from "../assets/MdiInstagram.svg"
+import WhatsApp from "../assets/MdiWhatsapp.svg"
+import Gmail from "../assets/BxBxlGmail.svg"
 
-function Footer(){
-    return(
-        <footer className="bg-custom-gray text-black py-6">
+function Footer() {
+  return (
+    <footer className="bg-custom-gray text-black py-6">
       <div className="container mx-auto text-center">
         <hr className="border-gray-400 mb-4" />
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
+          <div className="mx-auto text-left mt-5">
+            <div className="mb-2">
+              <NavLink to="/">Inicio</NavLink>
+            </div>
+            <div className="mb-2">
+              <NavLink to="/Services">Nuestros Servicio</NavLink>
+            </div>
+            <div className="mb-2">
+              <NavLink to="/Blog">Blog</NavLink>
+            </div>
+            <div className="mb-2">
+              <NavLink to="/Comunidad">Comunidad Creva</NavLink>
+            </div>
+          </div>
           <div>
-            <img src={LogoBlanco} alt="Logo Footer" className="w-1/2  mx-auto" style={{width: `120px`}}/>
+            <img src={Logo} alt="Logo Footer" className="w-1/2  mx-auto" style={{ width: `90px` }} />
             <p className="mt-5">&copy; 2023 CreVa Group</p>
-                <p>Todos los derechos reservados.</p>
-          </div>
-          <div className="text-center">
-          <div className="mb-2">
-            <NavLink to="/">Inicio</NavLink>
-          </div>
-            <div className="mb-2">
-            <NavLink to="/Services">Nuestros Servicio</NavLink>
-            </div>
-            <div className="mb-2">
-            <NavLink to="/Team">Equipo</NavLink>
-            </div>
-            <div className="mb-2">
-            <NavLink to="/Blog">Blog</NavLink>
-            </div>
-            <div className="mb-2">
-            <NavLink to="/Comunidad">Comunidad Creva</NavLink>
-            </div>
-            <div className="mb-2">
-            <NavLink to="/Contact">Contacto</NavLink>
-            </div>
-            <div className="mb-2"></div>
-          </div>
-          <div>
-            <div className="mb-2">
-            <NavLink to="https://www.instagram.com/creva.group/">Instagram</NavLink>
-            </div>
-            <div className="mb-2">
-            <NavLink to="https://www.linkedin.com/company/creva-group/">Linkedin</NavLink>
-            </div>
+            <p>Todos los derechos reservados.</p>
           </div>
         </div>
-        
+        <div className="flex justify-center mt-2">
+          <div className="mx-4">
+            <a href="https://www.instagram.com/creva.group/" target="_blank">
+              <img src={Instagram} alt="Instagram" className="" style={{ width: `40px` }} />
+            </a>
+          </div>
+          <div className="mx-4">
+            <a href="https://www.linkedin.com/company/creva-group/" target="_blank">
+              <img src={Linkedin} alt="Linkedin Logo" style={{ width: `40px` }} />
+            </a>
+          </div>
+          <div className="mx-4">
+            <a href={`mailto:crevagroup.global@gmail.com`} target="_blank">
+              <img src={Gmail} alt="Gmail" className="" style={{ width: `40px` }} />
+            </a>
+          </div>
+          <div className="mx-4">
+            <a href="https://api.whatsapp.com/send?phone=5491167867530&text=%C2%A1Hola%21+Quiero+conocer+m%C3%A1s+sobre+los+servicios+de+CreVa+Group." target="_blank">
+              <img src={WhatsApp} alt="WhatsApp" className="" style={{ width: `40px` }} />
+            </a>
+          </div>
+        </div>
       </div>
     </footer>
-    )
+  )
 }
 
 export default Footer;
