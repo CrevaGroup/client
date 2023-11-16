9a010f3e7a41a4c9ace75ea1cf4e510a48197e59
ValentinoJerez <valenjerez69@gmail.com>
2023-11-16 00:37:42 -0300

    CambiosCarrusel

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -33,7 +33,7 @@ function Carrusel() {
             className="block w-full h-96 object-cover rounded-3xl"
             alt="Fondo 1"
           />
-          <span className="absolute p-8 inset-0 flex items-center justify-center text-2xl font-semibold text-black sm:text-3xl dark:text-gray-800">
+          <span className="absolute p-8 inset-0 flex items-center justify-center text-2xl font-semibold text-white sm:text-3xl dark:text-gray-800">
             Muchos profesionales se enfrentan a un desafío común: no cuentan con
             las herramientas necesarias para buscar trabajo de manera efectiva.
           </span>
@@ -44,7 +44,7 @@ function Carrusel() {
             className="block w-full h-96 object-cover rounded-3xl"
             alt="Fondo 2"
           />
-          <span className="absolute p-8 inset-0 flex items-center justify-center text-2xl font-semibold text-black sm:text-3xl dark:text-gray-800">
+          <span className="absolute p-8 inset-0 flex items-center justify-center text-2xl font-semibold text-white sm:text-3xl dark:text-gray-800">
             ¿Cómo redactar un currículum que destaque? ¿Cómo generar el impacto
             deseado en una entrevista? ¿Cómo conseguir que las empresas te
             encuentren en LinkedIn?
@@ -56,7 +56,7 @@ function Carrusel() {
             className="block w-full h-96 object-cover rounded-3xl"
             alt="Fondo 3"
           />
-          <span className="absolute p-8 inset-0 flex items-center justify-center text-2xl font-semibold text-black sm:text-3xl dark:text-gray-800">
+          <span className="absolute p-8 inset-0 flex items-center justify-center text-2xl font-semibold text-white sm:text-3xl dark:text-gray-800">
             En Creva estamos para brindar esas respuestas e impulsar a la gente
             a alcanzar su máximo potencial
           </span>
