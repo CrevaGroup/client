81efa440555b0600d628aad91cd73a0bc77d423f
ValentinoJerez <valenjerez69@gmail.com>
2023-11-16 01:28:35 -0300

    ArregloFailure

--- src/views/Failure.jsx
+++ src/views/Failure.jsx
@@ -16,7 +16,7 @@ function Failure() {
             <div className="flex flex-col lg:flex-row items-center justify-center">
                 <div className="lg:w-1/2 mt-8">
                     <h1 className="text-4xl flex justify-center">Oops! Algo ha fallado</h1>
-                    <div className="mt-5 flex justify-center">
+                    <div className=" flex justify-center">
                         <Button onClick={navigateServices} className="mt-5 bg-dark-violet py-2 px-4">Nuestros Servicios</Button>
                     </div>
                 </div>
