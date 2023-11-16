6afbc6cf6ee63a5e0644ebed4ad6d12d12b05086
lopezsalvaro16 <alvaro154020747@gmail.com>
2023-11-16 09:35:28 -0300

    centre e hice responsive estas vistas y le puse dark mode

--- src/views/Failure.jsx
+++ src/views/Failure.jsx
@@ -1,31 +1,38 @@
 import { useNavigate } from "react-router-dom";
 
-import { Button } from "@material-tailwind/react"
+import { Button } from "@material-tailwind/react";
 
-import Error from "../assets/400-Error-Bad Request-pana.png"
+import Error from "../assets/400-Error-Bad Request-pana.png";
 
 function Failure() {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    function navigateServices() {
-        navigate("/Services");
-    }
+  function navigateServices() {
+    navigate("/Services");
+  }
 
-    return (
-        <div className="bg-custom-gray">
-            <div className="flex flex-col lg:flex-row items-center justify-center">
-                <div className="lg:w-1/2 mt-8">
-                    <h1 className="text-4xl flex justify-center">Oops! Algo ha fallado</h1>
-                    <div className="mt-5 flex justify-center">
-                        <Button onClick={navigateServices} className="mt-5 bg-dark-violet py-2 px-4">Nuestros Servicios</Button>
-                    </div>
-                </div>
-                <div className="lg:w-1/2">
-                    <img src={Error} alt="Error Image" style={{ width: `660px` }} />
-                </div>
-            </div>
+  return (
+    <div className="bg-custom-gray dark:bg-purple-900 dark:text-white h-screen">
+      <div className="flex flex-col lg:flex-row items-center justify-center h-full">
+        <div className="lg:w-1/2 mt-8">
+          <h1 className="text-4xl flex justify-center">
+            Oops! Algo ha fallado
+          </h1>
+          <div className="mt-5 flex justify-center">
+            <Button
+              onClick={navigateServices}
+              className="mt-5 bg-dark-violet py-2 px-4"
+            >
+              Nuestros Servicios
+            </Button>
+          </div>
         </div>
-    )
+        <div className="lg:w-1/2">
+          <img src={Error} alt="Error Image" style={{ width: `660px` }} />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Failure;
--- src/views/Success.jsx
+++ src/views/Success.jsx
@@ -1,29 +1,37 @@
 import { useNavigate } from "react-router-dom";
-import SuccessShop from "../assets/Successful-purchase-cuate.png"
+import SuccessShop from "../assets/Successful-purchase-cuate.png";
 import { Button } from "@material-tailwind/react";
 
 function Success() {
-    const navigate = useNavigate()
+  const navigate = useNavigate();
 
-    function navigateHome(){
-        navigate("/")
-    }
+  function navigateHome() {
+    navigate("/");
+  }
 
-    return (
-        <div className="bg-custom-gray">
-            <div className="flex flex-col lg:flex-row items-center justify-center">
-                <div className="lg:w-1/2 mt-8">
-                    <h1 className="text-4xl flex justify-center ml-12">Tu compra ha sido realizada con éxito</h1>
-                    <div className="mt-5 flex justify-center">
-                        <Button className="bg-dark-violet" onClick={navigateHome}>Volver al inicio</Button>
-                    </div>
-                </div>
-                <div className="lg:w-1/2">
-                    <img src={SuccessShop} alt="Compra Exitosa" style={{width:`660px`}} />
-                </div>
-            </div>
+  return (
+    <div className="bg-custom-gray dark:bg-purple-900 dark:text-white h-screen">
+      <div className="flex flex-col lg:flex-row items-center justify-center h-full">
+        <div className="lg:w-1/2 mt-8 flex flex-col justify-center items-center">
+          <h1 className="text-4xl text-center lg:text-left">
+            Tu compra ha sido realizada con éxito
+          </h1>
+          <div className="mt-5 flex justify-center align-center">
+            <Button className="bg-dark-violet" onClick={navigateHome}>
+              Volver al inicio
+            </Button>
+          </div>
         </div>
-    )
+        <div className="lg:w-1/2 flex justify-center items-center mt-8 lg:mt-0">
+          <img
+            src={SuccessShop}
+            alt="Compra Exitosa"
+            className="max-w-full h-auto"
+          />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Success;
