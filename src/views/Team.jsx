3c4df0766114c81473b8d4aa863765e6437518f4
ValentinoJerez <valenjerez69@gmail.com>
2023-11-15 23:41:30 -0300

    ListoEquipo

--- src/views/Team.jsx
+++ src/views/Team.jsx
@@ -6,7 +6,7 @@ import Linkedin from "../assets/Linkedin.svg"
 import Instagram from "../assets/Instagram.svg"
 import Behance from "../assets/PhBehanceLogo.svg"
 import Alvaro from "../assets/Alvaro.png"
-import Martin from "../assets/Martin.jpg"
+import Martin from "../assets/Martin2.jpg"
 import Osvaldo from "../assets/Osvaldo.png"
 
 function Team() {
@@ -18,7 +18,7 @@ function Team() {
       <div className="flex flex-col lg:flex-row lg:items-center justify-center">
         <div className="mx-4 mt-5 mb-5">
           <div className="flex items-center p-3 lg:w-72 h-28 bg-white rounded-md shadow-lg">
-            <img src={Alvaro} alt="Alvaro Image" className="flex justify-center items-center w-14 h-14 rounded-full shadow-md hover:cursor-pointer hover:scale-110 duration-300"/>
+            <img src={Alvaro} alt="Alvaro Image" className="flex justify-center items-center w-14 h-14 rounded-full shadow-md hover:cursor-pointer hover:scale-110 duration-300" />
             <section className="block border-l border-gray-300 m-3">
               <div className="pl-3">
                 <h3 className="text-gray-600 font-semibold text-sm">Alvaro Mauricio Lopez</h3>
@@ -93,7 +93,7 @@ function Team() {
       <div className="flex flex-col lg:flex-row lg:items-center justify-center">
         <div className="mx-4 mt-5 mb-5">
           <div className="flex items-center p-3 lg:w-72 h-28 bg-white rounded-md shadow-lg">
-          <section className="flex justify-center items-center w-14 h-14 rounded-full shadow-md bg-gradient-to-r from-[#F9C97C] to-[#A2E9C1] hover:from-[#C9A9E9] hover:to-[#7EE7FC] hover:cursor-pointer hover:scale-110 duration-300">
+            <section className="flex justify-center items-center w-14 h-14 rounded-full shadow-md bg-gradient-to-r from-[#F9C97C] to-[#A2E9C1] hover:from-[#C9A9E9] hover:to-[#7EE7FC] hover:cursor-pointer hover:scale-110 duration-300">
               <img src={User} alt="User Image" className="w-7 fill-gray-700" />
             </section>
             <section className="block border-l border-gray-300 m-3">
@@ -115,45 +115,43 @@ function Team() {
 
         <div className="mx-4 mt-5 mb-5">
           <div className="flex items-center p-3 lg:w-72 h-28 bg-white rounded-md shadow-lg">
-          <section className="flex justify-center items-center w-14 h-14 rounded-full shadow-md bg-gradient-to-r from-[#F9C97C] to-[#A2E9C1] hover:from-[#C9A9E9] hover:to-[#7EE7FC] hover:cursor-pointer hover:scale-110 duration-300">
-                <img src={User} alt="User Image" className="w-7 fill-gray-700" />
-              </section>
-              <section className="block border-l border-gray-300 m-3">
-                <div className="pl-3">
-                  <h3 className="text-gray-600 font-semibold text-sm">Martin Garcia Hervas</h3>
-                  <h3 className="bg-clip-text text-transparent bg-gradient-to-l from-[#005BC4] to-[#27272A] text-md font-bold">Developer Full Stack</h3>
-                </div>
-                <div className="flex gap-3 pt-2 pl-3">
-                  <a href=" https://github.com/MartinGarciaHervas" target="_blank">
-                    <img src={GitHub} alt="GitHub Logo" className="w-4 hover:scale-125 duration-200 hover:cursor-pointer fill-white stroke-2" />
-                  </a>
-                  <a href="https://www.linkedin.com/in/martin-garcia-hervas-07846a281/" target="_blank">
-                    <img src={Linkedin} alt="Linkedin Logo" className="w-4 hover:scale-125 duration-200 hover:cursor-pointer fill-white stroke-2" />
-                  </a>
-                </div>
-              </section>
+            <img src={Martin} alt="Martin Image" className="flex justify-center items-center w-14 h-14 rounded-full shadow-md hover:cursor-pointer hover:scale-110 duration-300"/>
+            <section className="block border-l border-gray-300 m-3">
+              <div className="pl-3">
+                <h3 className="text-gray-600 font-semibold text-sm">Martin Garcia Hervas</h3>
+                <h3 className="bg-clip-text text-transparent bg-gradient-to-l from-[#005BC4] to-[#27272A] text-md font-bold">Developer Full Stack</h3>
+              </div>
+              <div className="flex gap-3 pt-2 pl-3">
+                <a href=" https://github.com/MartinGarciaHervas" target="_blank">
+                  <img src={GitHub} alt="GitHub Logo" className="w-4 hover:scale-125 duration-200 hover:cursor-pointer fill-white stroke-2" />
+                </a>
+                <a href="https://www.linkedin.com/in/martin-garcia-hervas-07846a281/" target="_blank">
+                  <img src={Linkedin} alt="Linkedin Logo" className="w-4 hover:scale-125 duration-200 hover:cursor-pointer fill-white stroke-2" />
+                </a>
+              </div>
+            </section>
           </div>
         </div>
 
         <div className="mx-4 mt-5 mb-5">
           <div className="flex items-center p-3 lg:w-72 h-28 bg-white rounded-md shadow-lg">
-          <section className="flex justify-center items-center w-14 h-14 rounded-full shadow-md bg-gradient-to-r from-[#F9C97C] to-[#A2E9C1] hover:from-[#C9A9E9] hover:to-[#7EE7FC] hover:cursor-pointer hover:scale-110 duration-300">
-                <img src={User} alt="User Image" className="w-7 fill-gray-700" />
-              </section>
-              <section className="block border-l border-gray-300 m-3">
-                <div className="pl-3">
-                  <h3 className="text-gray-600 font-semibold text-sm">Osvaldo Corneli Nassif</h3>
-                  <h3 className="bg-clip-text text-transparent bg-gradient-to-l from-[#005BC4] to-[#27272A] text-md font-bold">Developer Full Stack</h3>
-                </div>
-                <div className="flex gap-3 pt-2 pl-3">
-                  <a href="https://github.com/OsvaldoCorneli" target="_blank">
-                    <img src={GitHub} alt="GitHub Logo" className="w-4 hover:scale-125 duration-200 hover:cursor-pointer fill-white stroke-2" />
-                  </a>
-                  <a href="https://www.linkedin.com/in/osvaldo-corneli-nassif-822728268/" target="_blank">
-                    <img src={Linkedin} alt="Linkedin Logo" className="w-4 hover:scale-125 duration-200 hover:cursor-pointer fill-white stroke-2" />
-                  </a>
-                </div>
-              </section>
+            <img src={Osvaldo} alt="Osvaldo Image" className="flex justify-center items-center w-14 h-14 rounded-full shadow-md hover:cursor-pointer hover:scale-110 duration-300"/>
+              {/* <img src={User} alt="User Image" className="w-7 fill-gray-700" /> */}
+            
+            <section className="block border-l border-gray-300 m-3">
+              <div className="pl-3">
+                <h3 className="text-gray-600 font-semibold text-sm">Osvaldo Corneli Nassif</h3>
+                <h3 className="bg-clip-text text-transparent bg-gradient-to-l from-[#005BC4] to-[#27272A] text-md font-bold">Developer Full Stack</h3>
+              </div>
+              <div className="flex gap-3 pt-2 pl-3">
+                <a href="https://github.com/OsvaldoCorneli" target="_blank">
+                  <img src={GitHub} alt="GitHub Logo" className="w-4 hover:scale-125 duration-200 hover:cursor-pointer fill-white stroke-2" />
+                </a>
+                <a href="https://www.linkedin.com/in/osvaldo-corneli-nassif-822728268/" target="_blank">
+                  <img src={Linkedin} alt="Linkedin Logo" className="w-4 hover:scale-125 duration-200 hover:cursor-pointer fill-white stroke-2" />
+                </a>
+              </div>
+            </section>
           </div>
         </div>
       </div>
