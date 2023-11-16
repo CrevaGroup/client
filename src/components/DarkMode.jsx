5d9450d4e6fcc9ceaf2cfb65926c2a90f11dfe93
AxelChoque <axel_chooque@hotmail.com>
2023-11-16 08:32:31 -0300

    fixing darkMode

--- src/components/DarkMode.jsx
+++ src/components/DarkMode.jsx
@@ -22,20 +22,20 @@ function DarkMode() {
   };
   return (
     <div className={`z-20 h-0 fixed lg:absolute top-5 flex justify-end mx-6 w-[93%] bg-[#C3A3D0] lg:w-[100px] lg:right-[-1%]  lg:top-[10%]  ${location.pathname === "/dashboard/newservices" || location.pathname === "/dashboard/profile" || location.pathname === "/dashboard/sales" || location.pathname === "/dashboard/clients" || location.pathname === "/login" || location.pathname === "/register" ? "hidden":""}`}>
-      <label class="switch-button" for="switch">
-        <div class="switch-outer">
+      <label className="switch-button" htmlFor="switch">
+        <div className="switch-outer">
           <input
             id="switch"
             type="checkbox"
-            onClick={toggleTheme}
+            onChange={toggleTheme}
             className={`theme-switch__checkbox px-4 py-2 rounded focus:outline-none focus:ring focus:border-blue-300 ${
               theme === "dark" ? "bg-gray-800 text-white" : ""
             }`}
             checked={theme === "light"}
           />
-          <div class="button">
-            <span class="button-toggle"></span>
-            <span class="button-indicator"></span>
+          <div className="button">
+            <span className="button-toggle"></span>
+            <span className="button-indicator"></span>
           </div>
         </div>
       </label>
